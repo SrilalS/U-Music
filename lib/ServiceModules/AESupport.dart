04fc99845ae226d2a0f4bae33127002102abb7a4
Srilal Sachintha <37685396+SrilalS@users.noreply.github.com>
2020-04-22 19:39:14 +0530

    CKEA

--- lib/ServiceModules/AESupport.dart
+++
@@ -1,5 +0,0 @@
-import 'AudioEngine.dart';
-
-void autonext(){
-  nowPlayingSongIndex==(musicList.length-1) ? play(0):play(nowPlayingSongIndex+1);
-}
--- lib/Styles/Styles.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-RoundedRectangleBorder roundedRectangleBorder(size){
-  return RoundedRectangleBorder(borderRadius: BorderRadius.circular(size));
-}
-
-TextStyle textStyle(size){
-  return TextStyle(fontSize: size);
-}
