668ede59bac6123f8c7869ed1ac08bafdfd21552
Srilal Sachintha <SrilalSachintha@Gmail.com>
2020-08-31 12:55:16 +0530

    ccx

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:umusicv2/Styles/Styles.dart';
 import 'package:umusicv2/UI/Home.dart';
 
@@ -8,8 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
+    return GetMaterialApp(
+      //debugShowCheckedModeBanner: false,
       title: 'U Music',
       theme: ThemeData(
         brightness: Brightness.dark,
